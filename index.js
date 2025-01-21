6a07586efff55bef072c4992122a09a4e42acb5e
Chris M <christopher.meadus@keyin.com>
2025-01-21 15:52:46 -0400

    Password Function Update
    - created the generatePassword function
    - made sure it defaulted to an 8 digit password, all lowercase
    - configured the output to show a welcome and users generated password
    - added comments where they are needed for explination, line 26, 31, 39, 42

--- index.js
+++ index.js
@@ -16,16 +16,29 @@ const userChoice = arguments[0];
 //--symbols     >> setup symbols for password
 //--uppercase   >> setup uppercase for password
 
-
-
-
 //setup function: generatePassword
+function generatePassword() {
+  //set available characters, length and set password to empty.
+  const charDefault = "abcdefghijklmnopqrstuvwxyz";
+  let password = "";
+  let length = 8;
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charDefault.length);
+    password += charDefault[randomIndex];
+  }
+  return password
+}
+
 //generates a random password based on user input options.
 //default: Generate an 8-character password with lowercase letters.
 //customizable: Add options for length, uppercase, numbers, and symbols.
 
+//call function to set password
+const password = generatePassword();
 
-//welcome message to confirm the script is running.
+//welcome message to confirm the script is running and your password is generated.
+console.log("");
 console.log("Welcome to the Password Generator!");
-
-
+console.log(`Here is your new generated password: ${password}`);
+console.log("");
